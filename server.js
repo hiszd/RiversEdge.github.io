c128cd96fd375b3dd4d20db8332a27ef6049cf59
Zion Koyl <hiszd1@gmail.com>
2019-02-07 13:55:51 -0500

    07FEB-03

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var app = ex();
 var path = require('path');
 var router = ex.Router();
 
-app.use(ex.static("HTML"));
+const cacheTime = 86400000 * 30
+
+app.use(ex.static("HTML", { maxAge: cacheTime}));
 
 // Route for root
 router.get('/', function (req, res) {
