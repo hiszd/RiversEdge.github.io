2a0a974f26381980359a30c5f1f03d6840b8c438
Zion Koyl <hiszd1@gmail.com>
2019-02-11 17:22:29 -0500

    11FEB19-1723

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
 var ex = require('express');
 var app = ex();
-var path = require('path');
-var router = ex.Router();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
 
-const cacheTime = 86400000 * 30
+const cacheTime = 86400000 * 30;
 
 app.use(ex.static("HTML", { maxAge: cacheTime}));
 
 // Route for root
-router.get('/', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile("/HTML/index.html");
 });
 
-app.use('/', router);
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+	console.log('user disconnected');
+    });
+});
+
+fs.watchFile('./HTML/index.html', (curr, prev) => {
+    io.emit('reload');
+    console.log("index.html changed");
+});
+
+fs.watchFile('./HTML/index.js', (curr, prev) => {
+    io.emit('reload');
+    console.log("index.js changed");
+});
+
+fs.watchFile('./HTML/zion.min.css', (curr, prev) => {
+    io.emit('reload');
+    console.log("zion.min.css changed");
+});
 
 // Listen to port
-app.listen(8080);
-console.log("Listening on port 8080!");
+http.listen(8080, function(){
+    console.log("Listening on port 8080!");
+});
